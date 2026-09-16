510c231f2794c76bcdab26ea45e2aa2d0c6b40ce
atarisherwan <akosoran330@gmail.com>
2026-09-16 21:32:42 +0300

    Add files via upload

--- 700-900/903.js
+++ 700-900/903.js
@@ -15,7 +15,7 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 
-// 9.00
+// 9.03, 9.04
 
 export const pthread_offsets = new Map(Object.entries({
     'pthread_create' : 0x25510,
@@ -26,10 +26,10 @@ export const pthread_offsets = new Map(Object.entries({
     'pthread_exit' : 0x77a0,
 }));
 
-export const off_kstr = 0x7f6f27;
-export const off_cpuid_to_pcpu = 0x21ef2a0;
+export const off_kstr = 0x7f4ce7;
+export const off_cpuid_to_pcpu = 0x21eb2a0;
 
-export const off_sysent_661 = 0x1107f00;
-export const jmp_rsi = 0x4c7ad;
+export const off_sysent_661 = 0x1103f00;
+export const jmp_rsi = 0x5325b;
 
-export const patch_elf_loc = "./900/kpatch/900.elf"; // Relative to `../../lapse.mjs`
+export const patch_elf_loc = "./900/kpatch/903.elf"; // Relative to `../../lapse.mjs`
